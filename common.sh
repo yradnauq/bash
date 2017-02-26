b909c795db5deea569b9d8c65cb34fac198ae993
yradnauq <brainhendricks@gmail.com>
2017-02-26 17:12:56 -0500

    Added blank line and rule functions
    Added blank line and rule functions

--- common.sh
+++ common.sh
@@ -3,16 +3,20 @@
 # Common Bash Functions
 #
 
+blank_line() { echo "" ; }
+single_rule() { echo '----------------------------------------------------------------------' ; }
+double_rule() { echo '======================================================================' ; }
+
 convert_csh_to_sh() {
-	# Convert test checks:
-	# () to []
-	# == to =
-	# && to -a
-	# || to -o
-	COMMAND=""
-	echo "Converting $1 to bourne syntax"
-#	COMMAND=`echo "$1" | tr '()' '[]' | sed -e 's/\=\=/\=/g' -e 's/&&/-a/g' -e 's/||/-o/'g`
-	COMMAND=`echo "$1" | tr '()' ' ' | sed -e 's/\=\=/\=/g' -e 's/&&/-a/g' -e 's/||/-o/'g`
-	echo "${COMMAND}"
+  # Convert test checks:
+  # () to []
+  # == to =
+  # && to -a
+  # || to -o
+  COMMAND=""
+  echo "Converting $1 to bourne syntax"
+#  COMMAND=`echo "$1" | tr '()' '[]' | sed -e 's/\=\=/\=/g' -e 's/&&/-a/g' -e 's/||/-o/'g`
+  COMMAND=`echo "$1" | tr '()' ' ' | sed -e 's/\=\=/\=/g' -e 's/&&/-a/g' -e 's/||/-o/'g`
+  echo "${COMMAND}"
 }
 
